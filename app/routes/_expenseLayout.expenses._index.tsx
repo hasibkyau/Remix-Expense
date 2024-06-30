296b85feea26eb4a75b58da632b570aff1833f46
hasibkyau <hasibkyau.cses@gmail.com>
2024-06-30 21:53:40 +0600

    updated expense list

--- app/routes/_expenseLayout.expenses._index.tsx
+++ app/routes/_expenseLayout.expenses._index.tsx
@@ -1,8 +1,39 @@
 import { Link } from "@remix-run/react";
-import React from "react";
+import ExpensesList from "~/components/expenses/ExpensesList";
+import { FaPlus, FaDownload } from "react-icons/fa";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 12.99,
+    data: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 15.39,
+    data: new Date().toISOString(),
+  },
+];
 
 const Expenses = () => {
-  return <div className="m-3"></div>;
+  return (
+    <div className="m-3">
+      <section id="expenses-actions">
+        <Link to="add">
+          <FaPlus />
+          <span>Add Expense</span>
+        </Link>
+        <a href="/expenses/raw">
+          <FaDownload />
+          <span>Load Raw Data</span>
+        </a>
+      </section>
+
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </div>
+  );
 };
 
 export default Expenses;
--- app/routes/_expenseLayout.tsx
+++ app/routes/_expenseLayout.tsx
@@ -1,37 +1,12 @@
-import { Link, Outlet } from "@remix-run/react";
-import ExpensesList from "~/components/expenses/ExpensesList";
+import { Outlet } from "@remix-run/react";
 import expensesStyles from "~/styles/expenses.css";
 import ExpensesHeader from "~/components/navigation/ExpenseHeader";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "First Expense",
-    amount: 12.99,
-    data: new Date().toISOString(),
-  },
-  {
-    id: "e2",
-    title: "Second Expense",
-    amount: 15.39,
-    data: new Date().toISOString(),
-  },
-];
 
 const ExpenseLayout = () => {
   return (
     <>
       <ExpensesHeader />
-      {/* <ul className="p-2 bg-purple-400 text-red-50 flex gap-3">
-        <Link to={"./expenses/add"}>Add</Link>
-        <Link to={"./expenses/1"}>Edit</Link>
-        <Link to={"./expenses/analysis"}>Analysis</Link>
-        <Link to={"./expenses/raw"}>Export</Link>
-      </ul> */}
       <Outlet />
-      <main>
-        <ExpensesList expenses={DUMMY_EXPENSES} />
-      </main>
     </>
   );
 };
