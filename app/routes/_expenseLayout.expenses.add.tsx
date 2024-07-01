25131e89660ae1111412dd797db57468fb77c9b6
hasibkyau <hasibkyau.cses@gmail.com>
2024-07-01 20:06:29 +0600

    added prisma and crated server for adding expense

--- app/routes/_expenseLayout.expenses.add.tsx
+++ app/routes/_expenseLayout.expenses.add.tsx
@@ -1,18 +1,29 @@
-import { useNavigate } from "@remix-run/react";
+import { ActionFunctionArgs } from "@remix-run/node";
+import { redirect, useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
-import Modal from '~/components/util/Modal'
+import Modal from '~/components/util/Modal';
+import {addExpense} from '~/data/expenses.server';
 
 const AddExpenses = () => {
-  const navigate = useNavigate();
-  function closeHandler(){
-    //Navigate programatically
-    navigate('..');
-  }
-  return (
-    <Modal onClose={closeHandler}>
-      <ExpenseForm />
-    </Modal>
-  );
-};
+    const navigate = useNavigate();
+    function closeHandler() {
+      //Navigate programatically
+      navigate("..");
+    }
+    return (
+      <Modal onClose={closeHandler}>
+        <ExpenseForm />
+      </Modal>
+    );
+}
 
-export default AddExpenses;
+export default AddExpenses
+
+export async function action({request}: ActionFunctionArgs){
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+  console.log('ExpenseData:', expenseData, formData);
+  
+  await addExpense(expenseData);
+  return redirect('/expenses')
+}
