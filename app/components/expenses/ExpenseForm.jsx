b00eabd83156492e2c2f51527edd871df5b8e3c3
hasibkyau <hasibkyau.cses@gmail.com>
2024-07-02 17:53:00 +0600

    added api for getting single data

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -4,12 +4,26 @@ import {
   useActionData,
   useSubmit,
   useNavigation,
+  useLoaderData,
 } from "@remix-run/react";
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
   const validationErrors = useActionData();
   const navigation = useNavigation();
+  const expenseData = useLoaderData();
+
+  const defaultValues = expenseData
+    ? {
+        title: expenseData.title,
+        amount: expenseData.amount,
+        date: expenseData.date,
+      }
+    : {
+        title: "",
+        amount: "",
+        date: "",
+      };
 
   const isSubmitting = navigation.state !== "idle";
 
@@ -34,7 +48,14 @@ function ExpenseForm() {
     >
       <p>
         <label htmlFor="title">Expense Title</label>
-        <input type="text" id="title" name="title" required maxLength={30} />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          maxLength={30}
+          defaultValue={defaultValues.title}
+        />
       </p>
 
       <div className="form-row">
@@ -47,11 +68,20 @@ function ExpenseForm() {
             min="0"
             step="0.01"
             required
+            defaultValue={defaultValues.amount}
           />
         </p>
         <p>
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" max={today} required />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            required
+            defaultValue={
+              defaultValues.date ? defaultValues.date.slice(0, 10) : ""
+            }
+          />
         </p>
       </div>
       {validationErrors && (
--- app/routes/_expenseLayout.expenses.$id.tsx
+++ app/routes/_expenseLayout.expenses.$id.tsx
@@ -1,14 +1,17 @@
-import { useNavigate } from "@remix-run/react";
+import { ActionFunctionArgs } from "@remix-run/node";
+import { json, useNavigate } from "@remix-run/react";
 import React from "react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { getExpense } from "~/data/expenses.server";
 
 const ExpenseDetails = () => {
-    const navigate = useNavigate();
-    function closeHandler() {
-      //Navigate programatically
-      navigate("..");
-    }
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    //Navigate programatically
+    navigate("..");
+  }
   return (
     <Modal onClose={closeHandler}>
       <ExpenseForm />
@@ -17,3 +20,14 @@ const ExpenseDetails = () => {
 };
 
 export default ExpenseDetails;
+
+export async function loader({ params }: ActionFunctionArgs) {
+  const expenseId = params.id;
+  try {
+    const expense = await getExpense(expenseId);
+    return expense;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
